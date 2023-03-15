e91506cd06c039ec23d85dac43a0729f0934b066
Oleksandr Nikytenko <nika200382@gmail.com>
2023-03-15 17:21:57 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@components/Button";
 import { Text } from "@components/Text";
 import { Avatar } from "@components/Avatar";
